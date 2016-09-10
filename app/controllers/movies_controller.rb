eddd22435a7931ed9bfb8c6d71d8a097ab3ecea4
Josh DiCristo <joshua.dicristo@gmail.com>
2016-09-10 01:05:27 -0400

    Add movie functionality complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,12 @@
 class MoviesController < ApplicationController	
 	def index
+	end
 
+	def new
+	end
+
+	def create
+		Movie.create({movie_title: params[:movie][:movie_title], year: params[:movie][:year], runtime: params[:movie][:runtime], rating_id: params[:movie][:rating_id], seen: params[:movie][:seen], subtitles: params[:movie][:aubtitles], best_picture_nominee: params[:movie][:best_picture_nominee], best_picture_winner: params[:movie][:best_picture_winner]});
+		redirect_to movies_path
 	end
 end
