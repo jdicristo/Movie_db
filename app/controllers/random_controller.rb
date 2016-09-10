aa2f9cd5c58e5e2e4b92aa0d69d80b602a31f853
Josh DiCristo <joshua.dicristo@gmail.com>
2016-09-09 23:04:08 -0400

    We like to have fun, here

--- app/controllers/random_controller.rb
+++ app/controllers/random_controller.rb
@@ -1,7 +1,22 @@
 class RandomController < ApplicationController
 	def show
+		get_random_movie
+		get_date_range
+	end
+
+	def get_random_movie
 		movie_list = Movie.all
-		rand = rand(0..movie_list.count-1)
-		@movie = movie_list[rand]
+		@movie = movie_list.count == 0 ? false : movie_list[rand(0..movie_list.count-1)]
+	end
+
+	def get_date_range
+		movies_by_year = Movie.all.order(:year)
+		first = Movie.first.year/10*10
+		last = Movie.last.year/10*10
+
+		@decade_array = []
+		(first..last).step(10) do |decade|
+			@decade_array.push({"id": decade, "decade": decade.to_s+"s"})
+		end
 	end
 end
