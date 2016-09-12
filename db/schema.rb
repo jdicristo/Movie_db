31d2a68b826e92396cf7012f80b8d4e706df632c
Josh DiCristo <joshua.dicristo@gmail.com>
2016-09-12 11:37:32 -0400

    Begin filter form functionality

--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,7 @@ ActiveRecord::Schema.define(version: 20169909213106) do
   end
 
   create_table "directors", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
