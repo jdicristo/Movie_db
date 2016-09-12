032659d1c0b68fd26e8bc961b833826e8330fa1b
Josh DiCristo <joshua.dicristo@gmail.com>
2016-09-11 22:35:54 -0400

    Hook up movie poster api

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,15 @@
+require 'httparty'
 class Movie < ApplicationRecord
 	validates_numericality_of :year, greater_than: 1888, less_than: Time.now.year
 	validates_numericality_of :runtime, greater_than: 0
 	#validates :movie_title, :year, :best_picture_winner, :best_picture_nominee, :runtime, :seen, :foreign, presence: true
 	
 	after_save :winner_is_nominee
+
+	def poster
+		response = HTTParty.get("http://www.omdbapi.com/?t=" + URI.encode(movie_title) + "&y=" + year.to_s)
+		response["Poster"] ? response["Poster"] : false
+	end
 
 	def winner_is_nominee
 		best_picture_nominee = true if best_picture_winner?
