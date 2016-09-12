c14b81083b3d7accd8e10173fda6543035e11a25
Josh DiCristo <joshua.dicristo@gmail.com>
2016-09-11 21:14:07 -0400

    Add delete function

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,13 @@
 class MoviesController < ApplicationController	
 	def index
+		@movies = Movie.all
 	end
 
 	def new
 	end
 
 	def create
-		Movie.create(params[:movie]);
+		Movie.create(movie_params);
 		redirect_to movies_path
 	end
 
@@ -21,5 +22,16 @@ class MoviesController < ApplicationController
 
 	def movie_params
 		params.require(:movie).permit(:movie_title, :year, :best_picture_winner, :best_picture_nominee, :runtime, :seen, :subtitles, :rating_id)
+	end
+
+	def sort_list
+		logger.debug(params)
+	end
+
+	def destroy
+	    @movie = Movie.find(params[:id])
+	    @movie.destroy
+
+	    redirect_to movies_path
 	end
 end
