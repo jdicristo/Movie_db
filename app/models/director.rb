84ed5605a58a4f1c3db9bc5ef702acbada0c4fe7
Josh DiCristo <joshua.dicristo@gmail.com>
2016-09-12 23:22:59 -0400

    Header and beginning tag functions

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,2 +1,7 @@
 class Director < ApplicationRecord
+	#has_many :movie_directors, dependant: :destroy
+
+	def movies
+		MovieDirector.where(director_id: id).map{ |m| Movie.find(m.movie_id)}
+	end
 end
